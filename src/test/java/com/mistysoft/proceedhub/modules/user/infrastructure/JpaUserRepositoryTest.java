da18ac11be4c371fb6733eb7c04aea06343e1603
sumire25 <msumirer@unsa.edu.pe>
2024-12-09 11:42:50 -0500

    Unit Test for JpaUserRepository, implementing test for save method

--- src/test/java/com/mistysoft/proceedhub/modules/user/infrastructure/JpaUserRepositoryTest.java
+++ src/test/java/com/mistysoft/proceedhub/modules/user/infrastructure/JpaUserRepositoryTest.java
@@ -1,4 +1,48 @@
+package com.mistysoft.proceedhub.modules.user.infrastructure;
+
+import com.mistysoft.proceedhub.modules.user.domain.User;
+import com.mistysoft.proceedhub.modules.user.domain.UserId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 class JpaUserRepositoryTest {
-  
+
+    @Mock
+    private SpringDataUserRepository repository;
+
+    @InjectMocks
+    private JpaUserRepository jpaUserRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void save() {
+        User user = User.builder()
+                .id(new UserId("id"))
+                .username("username")
+                .email("email")
+                .password("password")
+                .roles(Set.of())
+                .build();
+        UserEntity userEntity = UserMapper.toEntity(user);
+
+        jpaUserRepository.save(user);
+
+        ArgumentCaptor<UserEntity> userEntityCaptor = ArgumentCaptor.forClass(UserEntity.class);
+        verify(repository).save(userEntityCaptor.capture());
+        assertEquals(userEntity, userEntityCaptor.getValue());
+    }
 }
